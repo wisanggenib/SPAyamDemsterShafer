e0ae3c9732f7d286fe9785f8425ab37fa3b095b4
Wisanggenib <bagus.wisanggeni4897@gmail.com>
2020-03-21 21:09:49 +0700

    cek cek

--- koneksi.php
+++ koneksi.php
@@ -3,11 +3,10 @@
 $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
-$dbname='db_expert';
+$dbname='db_expert1111111';
 //-- database connections
 $db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 //-- halt and show error message if connection fail
 if ($db->connect_error) {
 	die('Connect Error ('.$db->connect_errno.')'.$db->connect_error);
 }
-?>
